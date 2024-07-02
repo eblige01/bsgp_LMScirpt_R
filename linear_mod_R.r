a76b5923cf84e6e1f9c2d7be1233727a4b1e4034
Elijah Blige <eblige@gmail.com>
2024-07-02 23:47:35 +0000

    updated code

--- linear_mod_R.r
+++ linear_mod_R.r
@@ -2,13 +2,12 @@ library(ggplot2)
 args = commandArgs()
 
 
-print("Please input a valid filename") 
+
 data = read.csv(args[6])
 print(data)
 ggplot(data , aes(x = x , y = y )) + geom_point() + xlab('X') + ylab('Y')
 
-model = lm(formula = y ~ x,
-               data = data)
+model = lm(formula = y ~ x, data = data)
 sum_stats = summary(model)
 sum_stats$r.squared
 coef(model)[1]
@@ -16,9 +15,9 @@ coef(model)[2]
 
 ggplot() +
   geom_point(aes(x = data$x, y = data$y),
-             #colour = 'red') + xlab('X') + ylab('Y') +
+             colour = 'red') + xlab('X') + ylab('Y') +
   geom_line(aes(x = data$x, y = predict(model, newdata = data)),
-            #colour = 'blue') 
+            colour = 'blue') 
 
 
 
