1403d2a50d9075cdd4e53c843ea4e8f98bb908c8
Elijah Blige <eblige@gmail.com>
2024-07-06 14:34:40 +0000

    Finishing the assignment

--- linear_mod_R.r
+++ linear_mod_R.r
@@ -1,23 +1,25 @@
+# Load libraries
 library(ggplot2)
+#Sets up input from terminal
 args = commandArgs()
 
 
-
+#Reading in the data file
 data = read.csv(args[6])
-print(data)
+#Making scatter plot
 ggplot(data , aes(x = x , y = y )) + geom_point() + xlab('X') + ylab('Y')
+ggsave("r_orig.png")
 
+#Running the linear model
 model = lm(formula = y ~ x, data = data)
 sum_stats = summary(model)
-sum_stats$r.squared
-coef(model)[1]
-coef(model)[2]
 
+# Making LM plot
 ggplot() +
   geom_point(aes(x = data$x, y = data$y),
              colour = 'red') + xlab('X') + ylab('Y') +
   geom_line(aes(x = data$x, y = predict(model, newdata = data)),
             colour = 'blue') 
-
+ggsave("r_lm.png")
 
 
